4ea0c8697accda0bad48e79850b1b53be041d160
DANUCKSAN <sathiyarajdanucksan@gmail.com>
2026-06-15 21:56:44 +1000

    hone-page-done

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -24,6 +24,23 @@ const BookCard = ({
         <p className="book-title">{title}</p>
         <p className="book-genre">{genre}</p>
       </div>
+      
+      {isLoanedBook && (
+        <div className="mt-3 w-full">
+          <div className="book-loaned">
+            <Image
+              src="/icons/calendar.svg"
+              alt="calendar"
+              width={18}
+              height={18}
+              className="object-contain"
+            />
+            <p className="text-light-100">11 days left to return</p>
+          </div>
+
+          <Button className="book-btn">Download receipt</Button>
+        </div>
+      )}
     </Link>
   </li>
 );
